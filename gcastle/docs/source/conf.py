9ce25a339d8afdd062ff3c36b3727898c9a30965
Keli Zhang <zhangkeli1@huawei.com>
2025-03-08 00:24:58 +0100

    tweak document generation

--- gcastle/docs/source/conf.py
+++ gcastle/docs/source/conf.py
@@ -5,7 +5,6 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
-print(os.path.abspath('../..'))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
